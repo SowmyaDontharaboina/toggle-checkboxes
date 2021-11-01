68360816c97efc3c479eb381c982f0ea88f4ae3e
Sowmya Dontharaboina <sow.anusha@gmail.com>
2021-11-01 09:13:01 +0530

    Added script file changes

---
+++ script.js
@@ -0,0 +1,26 @@
+const good = document.getElementById('good');
+const cheap = document.getElementById('cheap');
+const fast = document.getElementById('fast');
+
+const toggles = document.querySelectorAll('.toggle');
+
+
+
+const doTheTrick = (clicked) => {
+    if(good.checked && cheap.checked && fast.checked) {
+        if(good === clicked) {
+            fast.checked = false;
+        }
+        if(cheap === clicked) {
+            good.checked = false;
+        }
+        if(fast === clicked) {
+            cheap.checked = false;
+        }
+    }
+}
+toggles.forEach((toggle) => {
+    toggle.addEventListener('change', (e) => {
+        doTheTrick(e.target)
+    })
+})
